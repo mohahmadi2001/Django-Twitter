86f6ed91b243113f6208e901320304199cb1150d
Mohammad Mahdi Ahmadi <moh.ahmdi2001@gmail.com>
2023-06-20 13:59:35 +0330

    chore: base admin panel for content

--- contents/admin.py
+++ contents/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from .models import Post,Comment,Tag,Reaction,Image
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Reaction)
+class ReactionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
