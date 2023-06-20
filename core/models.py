bc0a936fb5839dd1195fe5c31c34f4a4cff0f337
Mohammad Mahdi Ahmadi <moh.ahmdi2001@gmail.com>
2023-06-20 13:14:59 +0330

    core model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from uuid import uuid4
 # Create your models here.
+
+class BaseModel(models.Model):
+    id = models.UUIDField(_("id"),editable=False,primary_key=True,default=uuid4)
+    
+    
+class TimStampMixin:
+    create_at = models.DateTimeField(_("create at"), auto_now=False, auto_now_add=True)
+    update_at = models.DateTimeField(_("update at"), auto_now=True, auto_now_add=False)
