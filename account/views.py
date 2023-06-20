6ac81056eba2648439bd81842f67cbecc10ea8db
Mohammad Mahdi Ahmadi <moh.ahmdi2001@gmail.com>
2023-06-20 16:26:49 +0330

    feat: user_list function in views

--- account/views.py
+++ account/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import User
 # Create your views here.
+
+def user_list(request):
+    users = User.get_users()
+    context = {'users': users}
+    return render(request, 'user_list.html', context)
