c8573181b0dcc738b1682f3ce9e68ad6b516ce7b
Mohammad Mahdi Ahmadi <moh.ahmdi2001@gmail.com>
2023-06-20 16:23:29 +0330

    feat: post_function in views

--- contents/views.py
+++ contents/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+def post_list(request):
+    posts = Post.get_posts()
+    context = {'posts': posts}
+    return render(request, 'post_list.html', context)
