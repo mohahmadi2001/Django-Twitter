2bf503e22c55e5cf65911fb49014cb19b0e254ff
Mohammad Mahdi Ahmadi <moh.ahmdi2001@gmail.com>
2023-06-27 11:40:01 +0330

    FollowUserView class in account view

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
-from .models import User
+from .models import User,Relation
 from django.views import View
+from django.contrib.auth.models import User
 # Create your views here.
 
 class UserView(View):
@@ -14,3 +15,19 @@ class ProfileView(View):
         user = get_object_or_404(User, username=username)
         profile = user.view_profile()
         return render(request,"profile.html",profile)
+    
+class FollowUserView(View):
+    def post(self, request, username):
+        user = get_object_or_404(User, username=username)
+        if request.user.is_authenticated:
+            if request.user != user:
+                if Relation.objects.follow(request.user, user):
+                    message = f'You are now following {username}'
+                else:
+                    message = f'You are already following {username}'
+            else:
+                message = 'You cannot follow yourself'
+        else:
+            message = 'You must be logged in to follow users'
+        
+        return render(request, 'follow_user.html', {'message': message})
