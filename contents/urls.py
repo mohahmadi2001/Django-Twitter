c63d10c364677ddd455e8211f93a494d4cd6f4f1
Mohammad Mahdi Ahmadi <moh.ahmdi2001@gmail.com>
2023-06-27 13:20:23 +0330

    CommentView class in content view

--- contents/urls.py
+++ contents/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('posts/',PostView.as_view(),name="post"),
     path('tags/',TagView.as_view(),name="tag"),
     path('images/',ImageView.as_view(),name="image"),
+    path('comments/',ImageView.as_view(),name="comment"),
 
 ]
--- contents/views.py
+++ contents/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Post,Tag,Image
+from .models import Post,Tag,Image,Comment,Reaction
 from django.views import View
 # Create your views here.
 
@@ -19,3 +19,9 @@ class ImageView(View):
     def get(self, request, image_id):
         image = get_object_or_404(Image, id=image_id)
         return render(request, 'image.html', {'image': image})
+
+
+class CommentView(View):
+    def get(self, request, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)
+        return render(request, 'comment.html', {'comment': comment})
