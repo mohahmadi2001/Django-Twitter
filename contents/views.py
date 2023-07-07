d73329ae1dfc1cffe2572827af1d32d88b9f6038
Mohammad Mahdi Ahmadi <moh.ahmdi2001@gmail.com>
2023-07-08 00:28:55 +0330

    contents forms

--- contents/views.py
+++ contents/views.py
@@ -31,3 +31,48 @@ class ReactionView(View):
     def get(self, request, reaction_id):
         reaction = get_object_or_404(Reaction, id=reaction_id)
         return render(request, 'reaction.html', {'reaction': reaction})
+    
+
+class AddTagView(View):
+    def post(self, request, post_id):
+        tag_text = request.POST.get('tag_text')
+        post = get_object_or_404(Tag, pk=post_id)
+        post.add_tag(tag_text)
+        return render(request,'post_detail.html', args=[post_id])
+
+
+class RemoveTagView(View):
+    def post(self, request, post_id):
+        tag_text = request.POST.get('tag_text')
+        post = get_object_or_404(Tag, pk=post_id)
+        post.remove_tag(tag_text)
+        return render(request,'post_detail.html', args=[post_id])
+
+
+class ArchivePostView(View):
+    def post(self, request, post_id):
+        post = get_object_or_404(Post, pk=post_id)
+        post.archive()
+        return render(request,'post_detail.html', args=[post_id])
+
+
+class UnarchivePostView(View):
+    def post(self, request, post_id):
+        post = get_object_or_404(Post, pk=post_id)
+        post.unarchive()
+        return render(request,'post_detail.html', args=[post_id])
+
+
+class AddImageView(View):
+    def post(self, request, post_id):
+        image_file = request.FILES.get('image_file')
+        post = get_object_or_404(Image, pk=post_id)
+        post.add_image(image_file)
+        return render(request,'post_detail.html', args=[post_id])
+
+
+class RemoveImageView(View):
+    def post(self, request, post_id, image_id):
+        post = get_object_or_404(Image, pk=post_id)
+        post.remove_image(image_id)
+        return render(request,'post_detail.html', args=[post_id])
